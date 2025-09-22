2f42692e441d7e120a9d43f0f01f62992f8c30e6
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-09-22 16:24:50 +0600

    Add style to NavBar

--- 01 React Fact Project/React Fact Project/src/components/NavBar.jsx
+++ 01 React Fact Project/React Fact Project/src/components/NavBar.jsx
@@ -1,22 +1,23 @@
 /** @format */
 
 import reactLogo from "../assets/react.svg";
+import "./NavBar.css";
 
 export default function NavBar() {
 	return (
 		<>
-			<a
-				style={{ display: "flex" }}
-				href="https://react.dev"
-				target="_blank"
-			>
-				<img
-					src={reactLogo}
-					className="logo react"
-					alt="React logo"
-				/>
-				<h1>ReactFacts</h1>
-			</a>
+			<header>
+				<nav>
+					<a href="https://react.dev" target="_blank">
+						<img
+							src={reactLogo}
+							className="logo react"
+							alt="React logo"
+						/>
+						<h1>ReactFacts</h1>
+					</a>
+				</nav>
+			</header>
 		</>
 	);
 }
