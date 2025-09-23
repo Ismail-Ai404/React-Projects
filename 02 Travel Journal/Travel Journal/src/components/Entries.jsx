0c3b53429d674117593786e1a2f9e2a7e07664da
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-09-23 22:11:36 +0600

    Add data

--- 02 Travel Journal/Travel Journal/src/components/Entries.jsx
+++ 02 Travel Journal/Travel Journal/src/components/Entries.jsx
@@ -1,18 +1,21 @@
 /** @format */
 
 import Entry from "./Entry";
+import visit from "../data/visit";
+
 export default function Entries() {
-	return (
-		<Entry
-			img={{
-				src: "https://scrimba.com/links/travel-journal-japan-image-url",
-				alt: "Mount Fuji",
-			}}
-			title="Mount Fuji"
-			country="Japan"
-			googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-			dates="12 Jan, 2021 - 24 Jan, 2021"
-			text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-		/>
-	);
+	const allVisits = visit.map((v, key = v.id) => {
+		return (
+			<Entry
+				img={v.img}
+				title={v.title}
+				country={v.country}
+				googleMapsLink={v.googleMapsLink}
+				text={v.text}
+				dates={v.dates}
+			/>
+		);
+	});
+
+	return allVisits;
 }
