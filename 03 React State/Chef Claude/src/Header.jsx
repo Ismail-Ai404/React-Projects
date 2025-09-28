2e683baae3f19ced2a8eee4c34223bdefa7def8e
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-09-28 21:15:36 +0600

    Add header

---
+++ 03 React State/Chef Claude/src/Header.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import chefClaudeLogo from "./assets/Chef Claude Icon.svg";
+
+export default function Header() {
+	return (
+		<header>
+			<img src={chefClaudeLogo} />
+			<h1>Chef Claude</h1>
+		</header>
+	);
+}
